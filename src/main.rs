1aff6296af6a382138c0fb589ccd036e26468c14
Niharika <niharika.3297@gmail.com>
2020-03-29 22:28:10 +0530

    Calculate score

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,80 @@
-const ALPHABETS : [char; 52] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-];
+use std::io;
+use std::collections::HashMap;
+use std::borrow::Borrow;
 
-const SCORES: [i8; 53] = [
-    1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
-    1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10,
-    1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
-    1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10,
-    0
-];
+fn main () {
+    let mut scrabble_score: HashMap<String, i32> = HashMap::new();
 
-fn main() {
-    // If word is CAT, score is 3+1+1 = 5
-    let letter = 'c';
-    match letter {
-        c => println! ("Found it! "),
-        _ => println! ("Did not find it.")
+    scrabble_score.insert("a".to_string(), 1);
+    scrabble_score.insert("b".to_string(), 3);
+    scrabble_score.insert("c".to_string(), 3);
+    scrabble_score.insert("d".to_string(), 2);
+    scrabble_score.insert("e".to_string(), 1);
+    scrabble_score.insert("f".to_string(), 4);
+    scrabble_score.insert("g".to_string(), 2);
+    scrabble_score.insert("h".to_string(), 4);
+    scrabble_score.insert("i".to_string(), 1);
+    scrabble_score.insert("j".to_string(), 8);
+    scrabble_score.insert("k".to_string(), 5);
+    scrabble_score.insert("l".to_string(), 1);
+    scrabble_score.insert("m".to_string(), 3);
+    scrabble_score.insert("n".to_string(), 1);
+    scrabble_score.insert("o".to_string(), 1);
+    scrabble_score.insert("p".to_string(), 3);
+    scrabble_score.insert("q".to_string(), 10);
+    scrabble_score.insert("r".to_string(), 1);
+    scrabble_score.insert("s".to_string(), 1);
+    scrabble_score.insert("t".to_string(), 1);
+    scrabble_score.insert("u".to_string(), 1);
+    scrabble_score.insert("v".to_string(), 4);
+    scrabble_score.insert("w".to_string(), 4);
+    scrabble_score.insert("x".to_string(), 8);
+    scrabble_score.insert("y".to_string(), 4);
+    scrabble_score.insert("z".to_string(), 10);
+
+    scrabble_score.insert("A".to_string(), 1);
+    scrabble_score.insert("B".to_string(), 3);
+    scrabble_score.insert("C".to_string(), 3);
+    scrabble_score.insert("D".to_string(), 2);
+    scrabble_score.insert("E".to_string(), 1);
+    scrabble_score.insert("F".to_string(), 4);
+    scrabble_score.insert("G".to_string(), 2);
+    scrabble_score.insert("H".to_string(), 4);
+    scrabble_score.insert("I".to_string(), 1);
+    scrabble_score.insert("J".to_string(), 8);
+    scrabble_score.insert("K".to_string(), 5);
+    scrabble_score.insert("L".to_string(), 1);
+    scrabble_score.insert("M".to_string(), 3);
+    scrabble_score.insert("N".to_string(), 1);
+    scrabble_score.insert("O".to_string(), 1);
+    scrabble_score.insert("P".to_string(), 3);
+    scrabble_score.insert("Q".to_string(), 10);
+    scrabble_score.insert("R".to_string(), 1);
+    scrabble_score.insert("S".to_string(), 1);
+    scrabble_score.insert("T".to_string(), 1);
+    scrabble_score.insert("U".to_string(), 1);
+    scrabble_score.insert("V".to_string(), 4);
+    scrabble_score.insert("W".to_string(), 4);
+    scrabble_score.insert("X".to_string(), 8);
+    scrabble_score.insert("Y".to_string(), 4);
+    scrabble_score.insert("Z".to_string(), 10);
+
+    // for (letter,score) in &scrabble_score {
+    //     match scrabble_score.get(letter) {
+    //         Some(score) => println!("{:?} : {:?}", letter, score),
+    //         None => println!("{:?} is not mentioned.", letter)
+    //     }
+    // }
+
+    let word = "cat".to_string();
+
+    let mut score : i32;
+
+    for letter in word.chars() {
+        let mut l = letter.to_string();
+        let mut score = match scrabble_score.get(&l) {
+            Some(score) => println!("{:?} : {:?}", l, score),
+            None => println!("{:?} is not mentioned.", l)
+        };
     }
-
 }
